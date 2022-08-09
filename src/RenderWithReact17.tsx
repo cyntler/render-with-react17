ff9a523e9b5d54a3c336185be7c7a89e7eac2d9a
Damian Cyntler <damian@cyntler.com>
2022-08-09 21:13:05 +0200

    Add main component

---
+++ src/RenderWithReact17.tsx
@@ -0,0 +1,47 @@
+import React, {
+  ComponentType,
+  FunctionComponent,
+  memo,
+  PropsWithChildren,
+  useEffect,
+  useRef,
+} from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { mockConsole, unmockConsole } from './consoleMocker';
+
+export interface RenderWithReact17Props {
+  wrapper?: ComponentType<PropsWithChildren>;
+}
+
+let RenderWithReact17Counter = 0;
+
+export const RenderWithReact17: FunctionComponent<
+  PropsWithChildren<RenderWithReact17Props>
+> = memo(({ children, wrapper: Wrapper }) => {
+  const divRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const divElement = divRef.current;
+
+    if (divElement && children) {
+      RenderWithReact17Counter++;
+      const node = Wrapper ? <Wrapper>{children}</Wrapper> : children;
+      if (typeof render === 'function') {
+        mockConsole();
+        render(<React.StrictMode>{node}</React.StrictMode>, divElement);
+        unmockConsole();
+      }
+    }
+
+    return () => {
+      RenderWithReact17Counter--;
+      if (typeof unmountComponentAtNode === 'function') {
+        unmountComponentAtNode(divElement);
+      }
+    };
+  }, []);
+
+  return (
+    <div id={`RenderWithReact17-${RenderWithReact17Counter}`} ref={divRef} />
+  );
+});
